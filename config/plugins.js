09619f4e9fdb1ddc428b6170a30202de21c53be4
Qamar <qazam@lucidya.com>
2023-06-15 18:23:00 +0300

    set up amazon s3 upload provider for images

--- config/plugins.js
+++ config/plugins.js
@@ -1,5 +1,21 @@
-module.exports = {
+module.exports = ({ env }) => ({
   upload: {
+    config: {
+      provider: "aws-s3",
+      providerOptions: {
+        accessKeyId: env("AWS_ACCESS_KEY_ID"),
+        secretAccessKey: env("AWS_ACCESS_SECRET"),
+        region: env("AWS_REGION"),
+        params: {
+          Bucket: env("AWS_BUCKET"),
+        },
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
+      },
+    },
     breakpoints: {
       xlarge: 1920,
       large: 1000,
@@ -8,4 +24,4 @@ module.exports = {
       xsmall: 64,
     },
   },
-};
+});
