f50d6f582aca346c391afcabd86f110fa2c92d98
Qamar <qazam@lucidya.com>
2023-07-13 19:56:51 +0300

    add http in CSP directives

--- config/middlewares.js
+++ config/middlewares.js
@@ -6,7 +6,7 @@ module.exports = [
       contentSecurityPolicy: {
         useDefaults: true,
         directives: {
-          "connect-src": ["'self'", "https:"],
+          "connect-src": ["'self'", "http:", "https:"],
           "img-src": [
             "'self'",
             "data:",
